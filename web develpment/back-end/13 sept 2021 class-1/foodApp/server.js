0050be3d13f6d9ee103dae2947691914ab0adf5a
Charandeep <ckaunsel123@gmail.com>
2021-09-14 00:11:15 +0530

    backend first class GET POST PUT PATCH DELETE

--- web develpment/back-end/13 sept 2021 class-1/foodApp/server.js
+++ web develpment/back-end/13 sept 2021 class-1/foodApp/server.js
@@ -6,8 +6,13 @@ app.listen('5000', function(){
     console.log('server listening on port 5000');
 });
 
-let user = {};
+// .use() fn -> middle ware function 
+app.use(express.json());
 
+let user = {};
+ 
+// get request -> to get data 
+// client <- data mangva raha hai 'server' sa
 app.get('/', (req, res) => {
    res.send('Home Page'); 
 });
@@ -15,3 +20,31 @@ app.get('/', (req, res) => {
 app.get('/user', (req, res) => {
     res.json(user);
 });
+
+// post request -> data bhejna
+// client se -> data bhej raha hai 'sever' koo
+app.post('/user', (req, res) => {
+    user = req.body; 
+    console.log(req.body);
+    res.send('data has been added sucessfully');
+});
+
+// patch request -> (to update value in existing object we have)
+app.patch('/user', (req, res) => {
+    let obj = req.body; // 
+
+    for(let key in obj){ // for in loop
+        user[key] = obj[key];
+    }
+
+    res.json(user);
+});
+
+
+// delete request -> 
+app.delete('/user', (req, res) => {
+    user = {};
+    res.json(user);
+    res.send('user have been deleted'); // to send this data to the user
+});
+
---
+++ web develpment/back-end/13 sept 2021 class-1/resource.md
@@ -0,0 +1,8 @@
+<!-- status in  -->
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+
+
+
+
+<!-- SIR GITHUB REPO LINK -->
+https://github.com/goelabhishek694/pep_backend
