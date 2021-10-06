00e3c853f60c35da532f9be0d7122f46222eba4f
Charandeep <ckaunsel123@gmail.com>
2021-10-06 12:57:49 +0530

    1 oct 2021 fri upto jwt implementation

--- web develpment/back-end/oct/1 oct 2021 fri backend_revision_3/app1.js
+++ web develpment/back-end/oct/1 oct 2021 fri backend_revision_3/app1.js
@@ -1,13 +1,17 @@
 // npm init -y
 // npm i express
 
-const express = require("express")
+const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("./secrets");
+const cookieParser = require("cookie-parser");
 
+// Server // route -> request -> response/file/..
 const app = express();
 
-// this line 
+// this line
 // post -> '/'
 // app.post("/", function(req, res){
 
@@ -16,7 +20,6 @@ const app = express();
 //     next(); // next middleware fn ko call kar doo
 // });
 
-
 // reserve a folder only from which client can access the files
 app.use(express.static("Frontend_folder"));
 
@@ -24,6 +27,7 @@ app.use(express.static("Frontend_folder"));
 // // always use me
 // // express json -> req.body add
 app.use(express.json());
+app.use(cookieParser());
 
 // app.post("/", function(req, res){
 
@@ -48,7 +52,6 @@ app.use(express.json());
 //     });
 // });
 
-
 // function -> route path
 // frontend -> req -> /
 // read data storage
@@ -58,135 +61,148 @@ let content = JSON.parse(fs.readFileSync("./data.json"));
 const userRouter = express.Router();
 const authRouter = express.Router();
 
-app.use('/user', userRouter);
-app.use('/auth', authRouter);
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
 
 userRouter
-    .route('/')
-    // localhost/user -> get
-    .get(protectRoute, getUsers)
-    // localhost/user -> post
-    // .post(bodyChecker, createUser); 
+  .route("/")
+  // localhost/user -> get
+  .get(protectRoute, getUsers);
+// localhost/user -> post
+// .post(bodyChecker, createUser);
 
+authRouter.route("/signup").post(bodyChecker, signupUser);
 
-authRouter.route('/signup')
-    .post(bodyChecker, signupUser);
+authRouter.route("/login").post(bodyChecker, loginUser);
 
-authRouter.route('/login')
-    .post(bodyChecker, loginUser);
+function getUsers(req, res) {
+  res.status(200).json({
+    message: content,
+  });
+}
 
-function getUsers(req, res){
-    res.status(200).json({
-        "message": content
-    });
-};
-
-function protectRoute(req, res, next){
+function protectRoute(req, res, next) {
+  try {
     console.log("reached body checker");
 
+    // cookie-parser
+    console.log("93", req.cookies);
+    jwt.verify();
+
+    let decryptedToken = jwt.verify(req.cookies.JWT, JWT_SECRET);
+    console.log("97", decryptedToken);
     //jwt -> verify everytime that if you are brining the token to get your response
-    let isallowed = true; 
+
+    // let isallowed = true;
     // isallowed -> true -> user aga jaa sakta hai next middleware fn pai i.e, getUser()
     //  isallowed -> false -> user aga nahi jaa sakta hai next middleware fn pai i.e, getUser()
-    if(isallowed) {
-        next();
+
+    if (decryptedToken) {
+      next();
     } else {
-        res.send("kindly login to access this resource");
-    };
-};
+      res.send("kindly login to access this resource");
+    }
+  } catch (err) {
+      res.status(200).json({
+          message: err.message,
+      });
+  }
+}
 
 function bodyChecker(req, res, next) {
-    console.log("reached body checker");
-    let isPresent = Object.keys(req.body).length;
+  console.log("reached body checker");
+  let isPresent = Object.keys(req.body).length;
 
-    console.log("ispresent", isPresent);
-    if (isPresent) {
-        next();
-    } else {
-        res.send("Kind send details in body");
-    }
+  console.log("ispresent", isPresent);
+  if (isPresent) {
+    next();
+  } else {
+    res.send("Kind send details in body");
+  }
 }
 
 function signupUser(req, res) {
-    let { name, email, password, confirmPassword } = req.body;
-    console.log("res.body", req.body);
+  let { name, email, password, confirmPassword } = req.body;
+  console.log("res.body", req.body);
 
-    if(password === confirmPassword) {
-        let newUser = { name, email, password } ;
+  if (password === confirmPassword) {
+    let newUser = { name, email, password };
 
-        // entery put
-        content.push(newUser);
-        console.log("content arr", content);
+    // entery put
+    content.push(newUser);
+    console.log("content arr", content);
 
-        //save in datastorage
-        fs.writeFileSync("data.json", JSON.stringify(content));
+    //save in datastorage
+    fs.writeFileSync("data.json", JSON.stringify(content));
 
-        res.status(201).json({
-            createUser: newUser
-        });
-    } else {
-        res.status(422).json({
-            message: "password and confirm password do not match"
-        });
-    };
+    res.status(201).json({
+      createUser: newUser,
+    });
+  } else {
+    res.status(422).json({
+      message: "password and confirm password do not match",
+    });
+  }
 }
 
 function loginUser(req, res) {
-    let { email, password } = req.body;
-    let obj = content.find((obj) => {
-        return obj.email == email
+  let { email, password } = req.body;
+  let obj = content.find((obj) => {
+    return obj.email == email;
+  });
+
+  if (!obj) {
+    return res.status(404).json({
+      message: "User not found",
     });
+  }
+  if (obj.password == password) {
+    let token = jwt.sign({ email: obj.email }, JWT_SECRET);
+    console.log(token);
+    //header
+    res.cookie("JWT", token);
 
-    if(!obj) {
-        return res.status(404).json({
-            message: "User not found",
-        });
-    }
-    if(obj.password == password){
-        res.status(200).json({
-            message: "user logged In",
-        });
-    } else {
-        res.status(422).json({
-            message: "password dosen't match",
-        });
-    };
-};
+    // sign with RSA SHA256
+    // res body
+    res.status(200).json({
+      message: "user logged In",
+    });
+  } else {
+    res.status(422).json({
+      message: "password dosen't match",
+    });
+  }
+}
 
-// userRouter  
+// userRouter
 //     .route("/:id")
 //     // localhost/user/10 -> post
 //     .get(getUser);
-
 
 // authRouter.route("/").post(signupUser);
 
 // authRouter.route("/:id").patch(forgetPassword);
 
-
 function createUser(req, res) {
-    console.log("create User");
+  console.log("create User");
 
-    let body = req.body;
-    console.log("req.body", req.body);
-    contentPush(body);
+  let body = req.body;
+  console.log("req.body", req.body);
+  contentPush(body);
 
-    // put data storage 
-    fs.writeFileSync("./data.json", JSON.stringify(content));
-    res.json({
-        message: content,
-    });
-};
-
-
+  // put data storage
+  fs.writeFileSync("./data.json", JSON.stringify(content));
+  res.json({
+    message: content,
+  });
+}
 
 app.listen(8081, function () {
-    console.log("server started");
+  console.log("server started");
 });
 
-
 // // --------------------------------------------
-// // explained next() concept here 
+// // explained next() concept here
 // app.post("/", function (req, res) {
 //     let body = req.body;
 //     console.log("inside first post ", body);
@@ -209,35 +225,28 @@ app.listen(8081, function () {
 //     res.send("tested next");
 // });
 
-
 // -------------------------------------------
 // 404 page
 app.use(function (req, res) {
-    let rest_of_the_path = path.join("./Frontend_folder", "404.html");
-    console.log("rest_of_the_path", rest_of_the_path);
-    
-    res.status(404).sendFile(path.join(__dirname, rest_of_the_path));
+  let rest_of_the_path = path.join("./Frontend_folder", "404.html");
+  console.log("rest_of_the_path", rest_of_the_path);
 
-    // res.status(404).sendFile(path.join(__dirname, "404.html"));
+  res.status(404).sendFile(path.join(__dirname, rest_of_the_path));
+
+  // res.status(404).sendFile(path.join(__dirname, "404.html"));
 });
 
-
-
 // --------------------------------------------
-
-
 
 // app.put("/", function(req, res){
 //     console.log("hello from home page");
 //     res.send("<h1>Hello from Backend</h1>");
 // });
 
-
 // app.update("/", function(req, res){
 //     console.log("hello from home page");
 //     res.send("<h1>Hello from Backend</h1>");
 // });
-
 
 // app.delete("/", function(req, res){
 //     console.log("hello from home page");
